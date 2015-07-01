921fadf55a36f7ac2069d264cb444360c0e9b5cf
mateuszkolasa <mateusz.kolasa@polcode.net>
2015-07-01 15:29:24 +0200

    Install Symfony2

---
+++ src/PolcodeProductBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PolcodeProductBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('PolcodeProductBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
